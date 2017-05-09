117aa9b9e1a8b221356b7af7451615e1bec66520
derrick-long <long.derrick8@gmail.com>
2017-05-09 11:57:31 -0400

    implemented advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -32,7 +32,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should accept valid addresses" do
-  valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                        first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
@@ -51,20 +51,24 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email addresses should be unique" do
-  duplicate_user = @user.dup
-  duplicate_user.email = @user.email.upcase
-  @user.save
-  assert_not duplicate_user.valid?
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
   end
 
   test "password should be present (nonblank)" do
-  @user.password = @user.password_confirmation = " " * 6
-  assert_not @user.valid?
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
   end
 
   test "password should have a minimum length" do
-  @user.password = @user.password_confirmation = "a" * 5
-  assert_not @user.valid?
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
   end
 
   # test "the truth" do
